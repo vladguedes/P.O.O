5513e12585ca7091f8ce2e93212dc14213110deb
vladguedes <vladguedes@hotmail.com>
2023-12-04 13:04:03 -0300

    feat: Questões respondidas. Questão 11 e 13.

---
+++ Unidade_1/Lista_1/Questao_12.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+/**
+ * Questao_12
+ */
+public class Questao_12 {
+
+    public static void main(String[] args) {
+        System.out.println("Em 2015, o mês de fevereio começou em um domingo. Digite um valor para saber em qual dia da semana cai.");
+        Scanner input = new Scanner(System.in);
+        int dia;
+
+        System.out.print("Digite um valor entre 1 e 28: ");
+        dia = input.nextInt();
+
+        for (int i = 1; i <= 28; i++) {
+            
+        }
+    }
+}
